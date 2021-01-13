f97d76d7886ecb2ee6a9936b460a194219356bfb
Brady Sutton <bsutt123@gmail.com>
2021-01-12 19:27:59 -0500

    Add additional tests to active directory problem

--- active_directory/problem_4.py
+++ active_directory/problem_4.py
@@ -57,9 +57,52 @@ other_child.add_user(other_child_user)
 child.add_group(sub_child)
 parent.add_group(child)
 
-print(is_user_in_group(sub_child_user, parent))                 # returns True
-print(is_user_in_group(other_child_user, parent))               # returns False
-print(is_user_in_group(sub_child_user, child))                  # returns True
-print(is_user_in_group(other_child_user, child))                # returns False
-print(is_user_in_group(other_child_user, other_child))          # returns True
-print(is_user_in_group(sub_child_user, sub_child))              # returns True
+print(is_user_in_group(sub_child_user, parent))  # returns True
+print(is_user_in_group(other_child_user, parent))  # returns False
+print(is_user_in_group(sub_child_user, child))  # returns True
+print(is_user_in_group(other_child_user, child))  # returns False
+print(is_user_in_group(other_child_user, other_child))  # returns True
+print(is_user_in_group(sub_child_user, sub_child))  # returns True
+
+empty_group = Group("empty")
+
+print(is_user_in_group("1", empty_group))  # returns False
+print(is_user_in_group("badinput", empty_group))  # returns False
+
+binary_tree_group = Group("root")
+left_group = Group("left")
+right_group = Group("right")
+
+binary_tree_group.add_group(left_group)
+binary_tree_group.add_group(right_group)
+
+right_group.add_user("right_user_1")
+right_group.add_user("right_user_2")
+right_group.add_user("right_user_3")
+left_group.add_user("left_user_1")
+left_group.add_user("left_user_2")
+left_group.add_user("left_user_3")
+
+print(is_user_in_group("right_user_1", binary_tree_group))  # returns True
+print(is_user_in_group("right_user_2", binary_tree_group))  # returns True
+print(is_user_in_group("right_user_3", binary_tree_group))  # returns True
+
+print(is_user_in_group("left_user_1", binary_tree_group))  # returns True
+print(is_user_in_group("left_user_2", binary_tree_group))  # returns True
+print(is_user_in_group("left_user_3", binary_tree_group))  # returns True
+
+print(is_user_in_group("left_user_1", right_group))  # returns False
+print(is_user_in_group("left_user_2", right_group))  # returns False
+print(is_user_in_group("left_user_3", right_group))  # returns False
+
+print(is_user_in_group("right_user_1", right_group))  # returns True
+print(is_user_in_group("right_user_2", right_group))  # returns True
+print(is_user_in_group("right_user_3", right_group))  # returns True
+
+print(is_user_in_group("left_user_1", left_group))  # returns True
+print(is_user_in_group("left_user_2", left_group))  # returns True
+print(is_user_in_group("left_user_3", left_group))  # returns True
+
+print(is_user_in_group("right_user_1", left_group))  # returns False
+print(is_user_in_group("right_user_2", left_group))  # returns False
+print(is_user_in_group("right_user_3", left_group))  # returns False
