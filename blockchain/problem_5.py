01db59584043b0d4b658e700824f7d004b551405
Brady Sutton <bsutt123@gmail.com>
2021-01-12 19:37:11 -0500

    Add test cases for blockchain

--- blockchain/problem_5.py
+++ blockchain/problem_5.py
@@ -34,6 +34,8 @@ class BlockChain:
         self.tail = block
 
     def __str__(self):
+        if self.root is None:
+            return ""
         current_node = self.root
         string = ""
         while current_node.next is not None:
@@ -47,14 +49,25 @@ class BlockChain:
 
 chain1 = BlockChain()
 chain2 = BlockChain()
+chain3 = BlockChain()
 
 chain1.append("somthing")
 chain1.append("something else")
 chain1.append("one last thing")
 
-chain2.append("somthing1")
-chain2.append("")
-chain2.append("one last thing1")
+chain2.append("thesamething")
+chain2.append("thesamething")
+chain2.append("thesamething")
+
+
+# I can't tell you exactly what these will look like but it will be roughly this
 
 print(chain1)
+
+# returns data: somthing, prev_hash: None, hash: 8ef0fbdd7478a8654251efe1791909cfc9f33f71b1427a3a4a85c3f1f5717adb -> data: something else, prev_hash: 8ef0fbdd7478a8654251efe1791909cfc9f33f71b1427a3a4a85c3f1f5717adb, hash: 395a23c58b1ee581a929f7ac9dda43f69019217a51f622330653a8047e3c9d88 -> data: one last thing, prev_hash: 395a23c58b1ee581a929f7ac9dda43f69019217a51f622330653a8047e3c9d88, hash: e5242b2562825ca60de226b5828a7e39a25e94eb74d36c359b33a3d0bd5c681c
+
 print(chain2)
+
+# returns data: thesamething, prev_hash: None, hash: 52819e76296aa8b77df0560a342333a5c96d309bfe34dc328ac93d0929dd29d4 -> data: thesamething, prev_hash: 52819e76296aa8b77df0560a342333a5c96d309bfe34dc328ac93d0929dd29d4, hash: cd360483bba2f2912ea561bfe49c3c6edd69bf68ed5a0d843e8b4dbb598f8bdb -> data: thesamething, prev_hash: cd360483bba2f2912ea561bfe49c3c6edd69bf68ed5a0d843e8b4dbb598f8bdb, hash: 46195923216e9224de75da8c58c618f09a8376cad9e3e7a05c8238549b14d212
+
+print(chain3) # returns ""
